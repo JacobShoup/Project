6e4426628db1f632a22f3fc3ed76c01c1b858d30
Jacob Shoup <02jacob.shoup@gulllakecs.org>
2019-03-11 09:57:59 -0400

    add roll again

--- Final Project.py
+++ Final Project.py
@@ -2,15 +2,23 @@
 #Roll a Dice
 
 
+# Imports the necessary modules
 import random
 
+
+# Sets the roll variable as true
 roll = True
 
+# Added "while' loop to ask for later use input
 while roll:
     print("rolling dice...")
     print("you rolled", random.randint(1, 6))
-    break
-
+    roll = input("Do you want to roll again? (yes or no): ")
+# Prints a number between 1 and 6 and asks if the user wants to roll again
+    if roll == "yes":
+        continue
+    else:
+        break
 
 
 
