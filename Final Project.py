4f1c05a4e8c7320a3b5baeeb39ca9263508c68a8
Jacob Shoup <02jacob.shoup@gulllakecs.org>
2019-03-11 09:42:16 -0400

    Add Roll Dice

--- Final Project.py
+++ Final Project.py
@@ -1,18 +1,15 @@
 #Jacob Shoup Final Project
 #Roll a Dice
 
+
 import random
-min = 1
-max = 6
 
-roll_again = "yes"
+roll = True
 
-while roll_again == "yes" or roll_again == "y":
-    print ("Rolling the die...")
-    print ("The Values are...")
-    print (random.randint(min, max))
-    print (random.randint(min, max))
-
+while roll:
+    print("rolling dice...")
+    print("you rolled", random.randint(1, 6))
+    break
 
 
 
