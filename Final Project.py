d6e986ac706345c4bf84107f9149beb224d77575
Jacob Shoup <02jacob.shoup@gulllakecs.org>
2019-03-11 09:24:33 -0400

    Add Variables

---
+++ Final Project.py
@@ -0,0 +1,23 @@
+#Jacob Shoup Final Project
+#Roll a Dice
+
+import random
+min = 1
+max = 6
+
+roll_again = "yes"
+
+while roll_again == "yes" or roll_again == "y":
+    print ("Rolling the die...")
+    print ("The Values are...")
+    print (random.randint(min, max))
+    print (random.randint(min, max))
+
+
+
+
+
+
+
+
+
