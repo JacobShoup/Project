677b5e8d34f9cc549bfb55f9d0dc6fe92d915675
Jacob Shoup <02jacob.shoup@gulllakecs.org>
2019-03-12 09:08:14 -0400

    Add Amount of Dice

--- Final Project.py
+++ Final Project.py
@@ -5,14 +5,15 @@
 # Imports the necessary modules
 import random
 
-
+dice = input("How many dice do you want to roll? ")
 # Sets the roll variable as true
 roll = True
 
 # Added "while' loop to ask for later use input
 while roll:
     print("rolling dice...")
-    print("you rolled", random.randint(1, 6))
+    for i in range (dice):
+        print("You rolled", random.randit(1, 6)))
     roll = input("Do you want to roll again? (yes or no): ")
 # Prints a number between 1 and 6 and asks if the user wants to roll again
     if roll == "yes":
